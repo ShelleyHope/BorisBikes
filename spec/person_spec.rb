54861d92ba133848f888e2bd4a27d53076690553
Shelley Hope <shelleyjhope@gmail.com>
2014-07-09 15:04:45 +0100

    Person can have a bike - test fails

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,5 +5,10 @@ describe 'Person' do
   it 'has no bike to begin with' do
     expect(Person.new).not_to have_bike
   end
+
+  it 'can have a bike' do
+    person = Person.new :bike
+    expect(person).to have_bike
+  end
   
 end
